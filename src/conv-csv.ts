16d61a63762a7954c7846016418599fffbeb6e2f
Gui Guy <jedandeganwang1999@gmail.com>
2023-12-04 15:50:02 -0600

    visualization

---
+++ src/conv-csv.ts
@@ -0,0 +1,21 @@
+import { stringify } from "csv-stringify/sync";
+import r from "../constructor-elos.json";
+import { writeFileSync } from "fs";
+
+const results = r as Record<string, number>[];
+const c = new Set<string>();
+for (const result of results)
+  for (const driver of Object.keys(result)) c.add(driver);
+
+process.stdout.write("cleaning... ");
+const latest = results[results.length - 1];
+for (const constructor of Object.keys(latest)) {
+  for (let i = results.length - 2; i >= 0; i--) {
+    if (results[i][constructor] === results[i + 1][constructor])
+      delete results[i + 1][constructor];
+  }
+}
+process.stdout.write("done\n");
+
+const columns = Array.from(c);
+writeFileSync("constructor.csv", stringify(results, { header: true, columns }));
